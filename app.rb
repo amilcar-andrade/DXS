b0766cc277110e220585c9e278c96a7f5b46308e
Amilcar Andrade <amilcar.andrade.g@gmail.com>
2011-12-23 15:19:02 -0600

    cambios

--- app.rb
+++ app.rb
@@ -1,3 +1,24 @@
 require "rubygems"
 require "bundler/setup"
 require "sinatra"
+
+
+get '/inicio' do
+  @titulo = "Inicio | Dexter Suasor"
+  erb :inicio
+end
+
+get '/empresa' do
+  @titulo = "Nuestras Certificaciones | Dexter Suasor"
+  erb :empresa
+end
+
+get '/contacto' do
+  @titulo = "Contacto | Dexter Suasor"
+  erb :contacto
+end
+
+get '/valores' do
+  @titulo = "Nuestra Empresa | Dexter Suasor"
+  erb:valores
+end

31603e0061d701a4f26ffb3d345505731d9056ef
Amilcar Andrade <amilcar.andrade.g@gmail.com>
2011-12-25 23:04:50 -0600

    Validacion Ruby

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "http://rubygems.org"
 
 gem "sinatra"
 gem "heroku"
+gem "sequel"
+gem "mysql"
--- app.rb
+++ app.rb
@@ -1,6 +1,38 @@
 require "rubygems"
 require "bundler/setup"
 require "sinatra"
+require "sequel"
+require "date"
+
+DB = Sequel.connect(:adapter => "mysql", :host=>"localhost", :user=>"root", :database=>"DXRS")
+
+DB.create_table?(:contacto) do
+  primary_key :id
+  String :name
+  String :compania
+  String :mail
+  String :telefono
+  String :estado
+  String :pregunta
+  DateTime :fecha
+
+end
+
+def llenaestados
+  estados = ['', 'Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guanajuato', 'Guerrero', 'Hidalgo', 'Jalisco', 'Mexico', 'Michoacan', 'Morelos', 'Nayarit', 'Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'San Luis Potosi', 'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatan', 'Zacatecas']
+  for i in 0...estados.length do
+    DB[:estado].insert(:name => estados[i])
+  end
+end
+
+
+DB.create_table?(:estado) do
+  primary_key :id
+  String :name
+
+end
+
+#llenaestados()
 
 
 get '/inicio' do
@@ -15,14 +47,18 @@ end
 
 get '/contacto' do
   @titulo = "Contacto | Dexter Suasor"
+  @estados = DB[:estado].all
   erb :contacto
 end
 
 get '/valores' do
   @titulo = "Nuestra Empresa | Dexter Suasor"
-  erb:valores
+  erb :valores
 end
 
-get '/popo' do
-  params.to_s
+get '/valida' do
+  DB.from(:contacto).insert(:fecha => DateTime.now, :name => params[:nombre], :compania => params[:compania], :mail => params[:mail],
+                            :telefono => params[:telefono], :estado => params[:estado], :pregunta => params[:pregunta])
+
+
 end

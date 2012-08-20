292012afa2f130fcbb5b7f58515156bc0d435ba7
Amilcar Andrade <amilcar.andrade@appian.com>
2012-08-20 15:44:20 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ if ENV['DATABASE_URL'] == nil
       :address => 'smtp.gmail.com',
       :port => '587',
       :enable_starttls_auto => true,
-      :user_name => 'amilcar.andrade.g',
+      :user_name => 'xxxx',
       :password => 'XXXXX',
       :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
       :domain => "localhost.localdomain" # the HELO domain provided by the client to the server
